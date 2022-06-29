b1218a3d8c5bef1136ee82812139f3c109c16a15
John Stray <johnstray@users.noreply.github.com>
2022-06-29 12:59:45 +0930

    Wrote functions for use in themes

--- plugins/ThemeExtras.php
+++ plugins/ThemeExtras.php
@@ -25,12 +25,64 @@ ThemeExtras_init();
 
 # Register this plugin with the system
 register_plugin(
-    THEMEXTRAS,
-    i18n_r(THEMEXTRAS . '/PLUGIN_NAME'),
-    THEMEXTRASVERS,
-    "John Stray",
-    i18n_r(THEMEXTRAS . '/AUTHOR_URL'),
-    i18n_r(THEMEXTRAS . '/PLUGIN_DESCRIPTION'),
-    'theme',
-    'ThemeXtras_main'
+    THEMEXTRAS,                                     // Plugin Identifier
+    i18n_r(THEMEXTRAS . '/PLUGIN_NAME'),            // Plugin Name
+    THEMEXTRASVERS,                                 // Plugin Version
+    "John Stray",                                   // Author's Name
+    i18n_r(THEMEXTRAS . '/AUTHOR_URL'),             // Author URL
+    i18n_r(THEMEXTRAS . '/PLUGIN_DESCRIPTION'),     // Plugin Description
+    'theme',                                        // Where the backend pages sit
+    'ThemeXtras_main'                               // Main backend controller function
 );
+
+# -----
+# Front-end Functions
+# -----
+
+/**
+ * Get page custom field
+ * Gets the value of the given custom field for the given page
+ *
+ * @since 1.0.0
+ * @param string $field The custom field to get the value for
+ * @param string $page (Optional) The page to get the custom field value from, default is the current page
+ * @param bool $echo (Optional) If true will echo the value to the page, default is true
+ * @return string The value of the custom field
+ */
+function get_page_custom_field( string $field, string $page = '', bool $echo = true ): string
+{
+    GLOBAL $TEMPLATE;
+
+    # If page is not given (empty), set it to the current page's slug
+    if ( empty($page) ) { $page = get_page_slug(false); }
+
+    # Get the XML data for the given page
+    $page_xml = getXML( GSDATAPAGESPATH . $page . '.xml' );
+
+    if ( isset($page_xml->{$TEMPLATE . '-' . $field}) )
+    {
+        return (string) $page_xml->{$TEMPLATE . '-' . $field};
+    }
+
+    return '';
+}
+
+/**
+ * Get theme config
+ * Gets the current configuration settings of the given theme. Uses current theme as the default
+ *
+ * @since 1.0.0
+ * @param string $theme (Optional) The theme to get the configuration for, default is the current theme
+ * @return array An array containing the currently configured settings of the given theme
+ */
+function get_theme_config( string $theme = '' ): array
+{
+    # If theme is not given (empty), set it to the current theme
+    if ( empty($theme) ) { GLOBAL $TEMPLATE; $theme = (string) $TEMPLATE; }
+
+    # Instansiate the core class so that we can use it here
+    $ThemeExtras = new ThemeExtras();
+
+    # Get the config from the class and return it
+    return $ThemeExtras->getConfig( $theme );
+}
